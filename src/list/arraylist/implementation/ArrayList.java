17a5188dbb207b8f2622d6f383bb5bbb59c29c91
Hyowon Kang <hyowon3434@naver.com>
2023-08-01 16:46:30 +0900

    Feat: add, addFirst함수 구현 및 실행

--- src/list/arraylist/implementation/ArrayList.java
+++ src/list/arraylist/implementation/ArrayList.java
@@ -10,10 +10,29 @@ public class ArrayList {
 
 
     // ArrayList 값 추가하는 메소드
+
+    // 1. 첫번째 인덱스에 값 추가
+    public boolean addFirst(Object element){
+        // add함수를 오출해서 0번째 인덱스를 입력하면 첫번째 인덱스에 값이 저장
+        return add(0, element);
+    }
+
+    // 마지막 인덱스에 값 추가
     public boolean addLast(Object element){
 
         elementData[size] = element;
         size++;
+        return true;
+    }
+
+    public boolean add(int index, Object element){
+
+
+        for (int i = size-1; i >= index; i--){ // 반복횟수는 맨 마지막 인덱스부터 사용자가 입력한 인덱스까지 1씩 감소
+            elementData[i+1] = elementData[i]; // 한칸씩 데이터를 오른쪽으로 이동
+        }
+        elementData[index] = element; // 입력된 데이터를 원하는 인덱스에 저장
+        size++; // ArrayList의 크기 증가
         return true;
     }
 }
--- src/list/arraylist/implementation/Main.java
+++ src/list/arraylist/implementation/Main.java
@@ -4,11 +4,19 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList numbers = new ArrayList();
+
+        // 마지막 인덱스에 값 추가
         numbers.addLast(10);
         numbers.addLast(20);
         numbers.addLast(30);
         numbers.addLast(40);
 
+        // 원하는 인덱스에 값 추가
+        // ArrayList는 값을 추가할 때 기존의 인덱스의 뒤에 있는 값을
+        // 모두 한 칸씩 밀려서 위치하기 때문에 시간이 많이 소요됨
+        numbers.add(1, 15);
 
+        // 첫번째 인덱스에 데이터 추가
+        numbers.addFirst(5);
     }
 }
