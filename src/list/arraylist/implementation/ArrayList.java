4752c90491429f66b1fae320451ce29139f833db
Hyowon Kang <hyowon3434@naver.com>
2023-08-01 16:16:41 +0900

    Feat: ArrayList add 메서드 구현 및 실행

--- src/list/arraylist/implementation/ArrayList.java
+++ src/list/arraylist/implementation/ArrayList.java
@@ -8,4 +8,12 @@ public class ArrayList {
     // 객체는 총 100개의 데이터를 담을 수 있게 설정
     private Object[] elementData = new Object[100];
 
+
+    // ArrayList 값 추가하는 메소드
+    public boolean addLast(Object element){
+
+        elementData[size] = element;
+        size++;
+        return true;
+    }
 }
--- src/list/arraylist/implementation/Main.java
+++ src/list/arraylist/implementation/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         ArrayList numbers = new ArrayList();
+        numbers.addLast(10);
+        numbers.addLast(20);
+        numbers.addLast(30);
+        numbers.addLast(40);
+
 
     }
 }
