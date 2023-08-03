6592c4487efb6a9019ebc4ac24d4cee48a9092a6
hyowon3434 <hyowon3434@naver.com>
2023-08-03 15:37:11 +0900

    Feat: add method

--- src/list/linkedlist/implementation/LinkedList.java
+++ src/list/linkedlist/implementation/LinkedList.java
@@ -1,5 +1,7 @@
 package list.linkedlist.implementation;
 
+import javax.jws.Oneway;
+
 public class LinkedList {
 
     private Node head;
@@ -11,6 +13,7 @@ public class LinkedList {
         private Object data;
 
         private Node next;
+
         public Node(Object input){
             this.data = input;
             this.next = null;
@@ -21,7 +24,6 @@ public class LinkedList {
 
     }
     // head에 값 추가
-
     public void addFirst(Object input) {
         Node newNode = new Node(input);
         newNode.next = head;
@@ -33,10 +35,11 @@ public class LinkedList {
     }
 
     // tail에 값 추가
+
     public void addLast(Object input) {
         Node newNode = new Node(input);
         if (size == 0){
-            addFirst(input);
+            addFirst(input); // LinkedList에 데이터가 없다면 addFirst 호출
         }else {
             tail.next = newNode;
             tail = newNode;
@@ -49,9 +52,29 @@ public class LinkedList {
     // node 메소드는 외부에 노출되면 안됨.
      /*public*/ Node node(int index){
         Node x = head;
-       for (int i = 0; i < index; i++){
-           x = x.next;
-       }
+        for (int i = 0; i < index; i++){
+            x = x.next;
+        }
         return x;
+    }
+
+    // 원하는 인덱스에 값 추가
+    public void add(int k, Object input) {
+        if (k == 0){
+            addFirst(input); // 인덱스값이 0이면 addFirst 함수 호출
+        }else {
+            Node temp1 = node(k - 1); // node 함수를 통해 원하는 인덱스 노드에 이전 노드가 어떤 값인지 알아낸다.
+            Node temp2 = temp1.next;
+            Node newNode = new Node(input);
+
+            temp1.next = newNode;
+            newNode.next = temp2;
+            size++;
+
+            if (newNode.next == null){
+                tail = newNode;
+            }
+
+        }
     }
 }
