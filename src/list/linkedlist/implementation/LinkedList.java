0fd9192f088ccca3791084dc580550dea21e1c1b
hyowon3434 <hyowon3434@naver.com>
2023-08-03 14:51:22 +0900

    Feat: addFirst 메서드 추가

--- src/list/linkedlist/implementation/LinkedList.java
+++ src/list/linkedlist/implementation/LinkedList.java
@@ -5,16 +5,29 @@ public class LinkedList {
     private Node head;
     private Node tail;
     private int size = 0;
+
     private class Node{
+
         private Object data;
         private Node next;
         public Node(Object input){
             this.data = input;
             this.next = null;
         }
-
         public String toString(){
             return String.valueOf(this.data);
+        }
+
+    }
+
+    // 값 추가
+    public void addFirst(Object input) {
+        Node newNode = new Node(input);
+        newNode.next = head;
+        head = newNode;
+        size++;
+        if (head.next == null){
+            tail = head;
         }
     }
 }
